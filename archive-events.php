1f60aca9cc01b50b1e3fae9209cef1c69cff3932
Reander Agulto <reander.agulto@oqulo.com>
2022-09-28 10:01:23 +0800

    Updated Events Page - Added Completed Events

--- archive-events.php
+++ archive-events.php
@@ -16,7 +16,7 @@ get_header();
 	<section id="content" class="hfeed">
 
         <?php
-            $upcomingArgs = [
+            $args = [
                 'post_type'      => 'events',
                 'post_status'    => 'future',
                 'order'          => 'ASC',
@@ -24,26 +24,12 @@ get_header();
                 'posts_per_page' => -1,
             ];
 
-            $completedArgs = [
-                'post_type'      => 'events',
-                'post_status'    => '',
-                'order'          => 'ASC',
-                'orderby'        => 'date',
-                'posts_per_page' => -1,
-                'date_query'     => array(
-                    //set date ranges with strings!
-                    'before'     => 'today',
-                    //allow exact matches to be returned
-                    'inclusive' => true,
-                ),
-            ];
-
             /* Upcoming Events */
-            $upcoming_events_query = new WP_Query( $upcomingArgs );
-            $upcoming_events_total = $upcoming_events_query->post_count;
-            if($upcoming_events_total > 0){
-                $upcoming_events_posts = $upcoming_events_query->posts;
-                $groupPosts = array_chunk( $upcoming_events_posts, 6 );
+            $events_query = new WP_Query( $args );
+            $events_total = $events_query->post_count;
+            if($events_total > 0){
+                $events_posts = $events_query->posts;
+                $groupPosts = array_chunk( $events_posts, 6 );
 
                 $group_content = '';
 
@@ -62,14 +48,12 @@ get_header();
                         $post_permalink = get_the_permalink( $post_id );
                         $post_thumbnail_url = get_the_post_thumbnail_url( $post_id, $thumbnail_size );
                         
-                        
-
                         $group_content .= '
                             <div class="event">
                                 <div class="event-container">
                                     <div class="img-container">
                                         <canvas width="350" height="299"></canvas>
-                                        <img src="' . $post_thumbnail_url . '" alt="' . $post_title . '" class="img-responsive" width="350" height="299" />
+                                        <img src="' . $post_thumbnail_url . '" alt="' . $post_title . '" width="350" height="299" />
                                     </div>
                                     <p class="event-date">' . $day . ' <span>' . $month . '</span></p>
                                 </div>
@@ -89,6 +73,79 @@ get_header();
                 wp_reset_query();
             }
         ?>
+
+        <?php 
+            $completredArgs = [
+                'post_type'      => 'events',
+                'post_status'    => 'publish',
+                'order'          => 'DESC',
+                'orderby'        => 'date',
+                'posts_per_page' => -1,
+                'date_query'     => array(
+                    //set date ranges with strings!
+                    'before'     => 'today',
+                    //allow exact matches to be returned
+                    'inclusive' => true,
+                ),
+            ];
+
+            /* Completed Events */
+            $events_query = new WP_Query( $completredArgs );
+            $events_total = $events_query->post_count;
+            if($events_total > 0){
+                $events_posts = $events_query->posts;
+                $groupPosts = array_chunk( $events_posts, 6 );
+                $group_content = '';
+
+                foreach( $groupPosts as $posts ) {
+
+                    $group_content .= '<section id="completed-events-section">
+                            <div class="completed-events-wrap">
+                                <h2 class="section-header text-center">Completed Events</h2>
+                                <div class="completed-events-content flex flex-wrap-wrap items-center">
+                    ';
+            
+                    foreach( $posts as $key => $post ) {
+                        $post_id = $post->ID;
+                        $post_title = $post->post_title;
+                        $post_date = date('F j, Y', strtotime($post->post_date));
+                        $post_permalink = get_the_permalink( $post_id );
+                        $post_thumbnail_url = get_the_post_thumbnail_url( $post_id, 'post-thumbnail' );
+            
+                        $group_content .= '
+                            <div class="completed-events">
+                                <a href="' . $post_permalink . '">
+                                    <div class="completed-event-container flex items-center justify-center">
+                                        <div class="img-container">
+                                            <canvas width="136" height="117"></canvas>
+                                            <img src="' . $post_thumbnail_url . '" alt="' . $post_title . '" width="136" height="117" />
+                                        </div>
+                                        <div class="events-content">
+                                            <h3>' . $post_title . '</h3>
+                                            <p>' . $post_date . '</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        ';
+                    }
+            
+                    $group_content .= '
+                            </div>
+                        </div>
+                    </div>
+                    ';
+                }
+            
+                echo $group_content;
+                wp_reset_postdata();
+                wp_reset_query();
+
+            }
+
+        ?>
+
+        
 
     </section><!-- end #content -->
 
