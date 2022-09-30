459dd24d7adcb9e69b12def1b4a323005ef0e016
Reander Agulto <reander.agulto@oqulo.com>
2022-09-30 15:02:20 +0800

    Updated Code - Added support for woocommerce

--- modules/woocommerce/module.php
+++ modules/woocommerce/module.php
@@ -1,7 +1,8 @@
 <?php 
 
-if ( ! class_exists('agentpro_woocommerce_support')){
-    class agentpro_woocommerce_support{
+if( !class_exists('add_woocommerce_support') ){
+    class add_woocommerce_support{
+
         /* 
         *
         * Function Name: __construct() 
@@ -10,15 +11,21 @@ if ( ! class_exists('agentpro_woocommerce_support')){
         function __construct() {
             add_theme_support( 'woocommerce', array(
                 'thumbnail_image_width' => 349,
+                'single_image_width'    => 541,
+        
                 'product_grid'          => array(
                     'default_rows'    => 3,
                     'min_rows'        => 2,
                     'max_rows'        => 8,
                     'default_columns' => 3,
-                    'min_columns'     => 2,
-                    'max_columns'     => 5,
+                    'min_columns'     => 1,
+                    'max_columns'     => 3,
                 ),
             ) );
         }
+
     }
+
+    $add_woocommerce_support = new add_woocommerce_support();
+    
 }
