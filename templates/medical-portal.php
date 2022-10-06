5009738032738938928b681fcfb1b167b433c0e4
Reander Agulto <reander.agulto@oqulo.com>
2022-10-06 08:39:47 +0800

    Added Merdical Portal CSS and Shareholders CSS

---
+++ templates/medical-portal.php
@@ -0,0 +1,284 @@
+<?php
+/*
+ * Template Name: Medical Portal
+ */
+get_header(); ?>
+
+<div id="ip-shareholders-portal">
+	<div class="ip-sp-container">
+		<div class="ip-sp-section">
+			<div class="ip-sp-hero-container">
+				<div class="ip-sp-hero-banner">
+					<canvas width="1440" height="315" style="background-image: url('<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/shareholders-banner.jpg');"></canvas>
+					<div class="ip-sp-hero-banner-title">
+						<div class="custom-container">
+							<h1>Medical Practitioner's Portal</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ip-sp-section">
+			<div class="ip-sp-slider-container">
+				<div class="custom-container">
+					<div class="ip-sp-slider-inner" data-aos="fade-up" data-aos-once="true">
+						<div class="ip-sp-slider">
+							<div class="ip-sp-slider-item">
+								<div class="ip-sp-slider-img site-img">
+									<canvas width="1100" height="400"></canvas>
+									<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/gallery-img-1.jpg" alt="Gallery Image">
+								</div>
+								<div class="ip-sp-slider-content-holder">
+									<div class="ip-sp-slider-title">
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<span>September 15, 2022</span>
+									</div>
+									<div class="ip-sp-slider-content">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+									</div>
+									<div class="ip-sp-slider-content-btn">
+										<a class="ip-sp-slider-btn ip-btn" href="#">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="ip-sp-slider-item">
+								<div class="ip-sp-slider-img site-img">
+									<canvas width="1100" height="400"></canvas>
+									<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/gallery-img-1.jpg" alt="Gallery Image">
+								</div>
+								<div class="ip-sp-slider-content-holder">
+									<div class="ip-sp-slider-title">
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<span>September 20, 2022</span>
+									</div>
+									<div class="ip-sp-slider-content">
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+									</div>
+									<div class="ip-sp-slider-content-btn">
+										<a class="ip-sp-slider-btn ip-btn" href="#">Read More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="ip-sp-slider-controls">
+							<button class="ip-sp-gallery-prev ai-font-arrow-b-p" aria-label="Prev"></button>
+							<button class="ip-sp-gallery-next ai-font-arrow-b-n" aria-label="Prev"></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ip-sp-section">
+			<div class="ip-sp-post-container">
+				<div class="custom-container">
+					<div class="ip-sp-post-tab-container" data-aos="fade-up" data-aos-once="true">
+						<div class="ip-sp-post-tab-holder">
+							<div class="ip-sp-post-tab-item active" data-tabaccess="All">
+								<a href="#">All</a>
+							</div>
+							<div class="ip-sp-post-tab-item" data-tabaccess="AAN News">
+								<a href="#">AAN News</a>
+							</div>
+							<div class="ip-sp-post-tab-item" data-tabaccess="Continuing Medical News + Events">
+								<a href="#">Continuing Medical News + Events</a>
+							</div>
+						</div>
+					</div>
+					<div class="ip-sp-post-content-container" data-aos="fade-up" data-aos-once="true">
+						<div class="ip-sp-post-holder" data-tabpanel="All">
+							<div class="ip-sp-post-inner-holder">
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="ip-sp-post-more-btn">
+								<a href="#" class="ip-btn">Show More Posts</a>
+							</div>
+						</div>
+						<div class="ip-sp-post-holder hidden-tab" data-tabpanel="AAN News">
+							<div class="ip-sp-post-inner-holder">
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="ip-sp-post-more-btn">
+								<a href="#" class="ip-btn">Show More Posts</a>
+							</div>
+						</div>
+						<div class="ip-sp-post-holder hidden-tab" data-tabpanel="Continuing Medical News + Events">
+							<div class="ip-sp-post-inner-holder">
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+								<div class="ip-sp-post-item">
+									<div class="ip-sp-post-img-holder">
+										<a href="#" class="ip-sp-post-img site-img">
+											<canvas width="348" height="212"></canvas>
+											<img src="<?php echo get_stylesheet_directory_uri();?>/images/shareholders-portal/post-img.jpg" alt="Post Image">
+										</a>
+									</div>
+									<div class="ip-sp-post-content">
+										<span>September 15, 2022</span>
+										<h2>Lorem ipsum dolor sit amet</h2>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...</p>
+										<a href="#" class="ip-sp-post-btn">Read More</a>
+									</div>
+								</div>
+							</div>
+							<div class="ip-sp-post-more-btn">
+								<a href="#" class="ip-btn">Show More Posts</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
