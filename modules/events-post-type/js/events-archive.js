e86a9ea74638b16de3a1a36aa04ed6e4dcfb90ae
Reander Agulto <reanderagulto29@gmail.com>
2022-10-10 15:08:53 +0800

    Updated Code

--- modules/events-post-type/js/events-archive.js
+++ modules/events-post-type/js/events-archive.js
@@ -4,37 +4,9 @@
          * Construct.
          */
         function __construct() {
-            eventsLoadMore();
         }
 
-        function eventsLoadMore(){
-            let $eventLoadMore = $('#completed-load-more');
-            let currentPage = 6;
-            let paged = 2;
-            let numPost = 0;
-            $eventLoadMore.on('click', function(e){
-                e.preventDefault();
-                paged++;
-                $.ajax({
-                    type: 'POST',
-                    url: '/wp-admin/admin-ajax.php',
-                    dataType: 'html',
-                    data: {
-                      action: 'completed_events',
-                      offset: currentPage + numPost,
-                      paged: paged,
-                    },
-                    success: function (res) {                        
-                        numPost = numPost + 3;
-                        $('.completed-events-content').append(res);
-                        let maxnumpage = parseInt($('#completed-events').val());
-                        if(paged == maxnumpage){
-                            $('.load-more-container').hide();
-                        }
-                    },
-                });
-            });
-        }
+
 
         /**
          * Instantiate
